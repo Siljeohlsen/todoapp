e64d127f4b74f6bab5cf40353aacb9806e46915d
charlottlt <charlottlt@uia.no>
2021-11-24 14:55:04 +0100

    jobbet mer

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const server = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 server.set("port", PORT);
 
 // middleware ---------------------------
