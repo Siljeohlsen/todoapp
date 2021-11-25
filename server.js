21a8e5a62b2a4f29d5fe40783c5224ecd6575ea0
Siljeohlsen <siljeoglucas@gmail.com>
2021-11-25 11:46:59 +0100

    Lagde database på Heroku og connected den til serveren

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const server = express();
 const PORT = process.env.PORT || 8080;
 server.set("port", PORT);
 
+const pg = require('pg');
+const tdaURI = "postgres://vdoxbxjaqxnsno:999f2f2993e7b8b9a34de8602571741c122cd539e136ad5d981b6a773b86dd5a@ec2-52-214-178-113.eu-west-1.compute.amazonaws.com:5432/ddghcl0kmg4ejf";
+const connstring = process.env.DATABASE_URL || tdaURI;
+const pool = new pg.Pool({
+	connectionString: connstring,
+	ssl: { rejectUnauthorized: false }
+});
+
+
 // middleware ---------------------------
 server.use(express.static("public"));
 server.use(express.json());
