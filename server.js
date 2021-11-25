ba783a767c2979c726dbb53e1af0b73c868eb9f4
Siljeohlsen <siljeoglucas@gmail.com>
2021-11-25 20:04:03 +0100

    Separerte koden for forskjellige routes

--- server.js
+++ server.js
@@ -4,68 +4,14 @@ const server = express();
 const PORT = process.env.PORT || 8080;
 server.set("port", PORT);
 
-const pg = require('pg');
-const tdaURI = "postgres://vdoxbxjaqxnsno:999f2f2993e7b8b9a34de8602571741c122cd539e136ad5d981b6a773b86dd5a@ec2-52-214-178-113.eu-west-1.compute.amazonaws.com:5432/ddghcl0kmg4ejf";
-const connstring = process.env.DATABASE_URL || tdaURI;
-const pool = new pg.Pool({
-	connectionString: connstring,
-	ssl: { rejectUnauthorized: false }
-});
+const lists = require("./modules/lists.js");
 
 
 // middleware ---------------------------
 server.use(express.static("public"));
 server.use(express.json());
 
-// endpoints ----------------------------
-server.get("/todoapp", async function(req, res, next) {
-	
-	try{
-		let data = await database.getAllLists();
-		res.status(200).json(data.rows).end();
-	}
-	catch(err) {
-		next(err);
-	}
-});
-
-server.post("/todoapp", async function(req, res, next) {
-
-	let updata = req.body;
-	let userid = 1;
-
-	try{
-		let data = await database.createLists(updata.heading, updata.listtext, userid);
-
-		if (data.rows.length > 0) {
-			res.status(200).json({msg: "The lists were created succesfully"}).end();
-		}
-		else{
-			throw "The lists couldn't be created";
-		}
-	}
-	catch(err) {
-		next(err);
-	}
-});
-
-server.delete("/todoapp", async function(req, res, next) {
-
-	let updata = req.body;
-
-	try{
-		let data = await database.deleteLists(updata.id);
-		if (data.rows.length > 0) {
-			res.status(200).json({msg: "The list was deleted succesfully"}).end();
-		}
-		else{
-			throw "The list couldn't be deleted";
-		}
-	}
-	catch(err) {
-		next(err);
-	}
-});
+server.use(lists);
 
 //error handling
 server.use(function (err, req, res, next) {
@@ -73,7 +19,6 @@ server.use(function (err, req, res, next) {
 		error: 'Something went wrong on the server!',
 		descr: err
 	}).end();
-
 });
 
 // start server ------------------------
