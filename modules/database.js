6579a19d75f73bfff33a089936b52fa6d07b25c8
Håkbook Air <air@Macbooks-Air-2.uia.no>
2021-11-26 15:42:20 +0100

    Lagt til funksjoner for å håndtere brukere i database.js

--- modules/database.js
+++ modules/database.js
@@ -29,6 +29,36 @@ databaseMethods.deleteLists = function(id) {
     return pool.query(sql, values); //return the promise
 }
 
+// Users -----------------------
+
+dbMethods.getAllUsers = function(){
+    let sql = "SELECT id, username FROM users";
+    return pool.query(sql); //return the promise
+}
+
+//-------------------------------
+dbMethods.getUser = function(username) {
+    let sql = "SELECT * FROM users WHERE username = $1";
+    let values = [username];
+    return pool.query(sql,values); //return the promise
+}
+
+//-------------------------------
+dbMethods.createUser = function(username, password, salt){
+    let sql = "INSERT INTO users (id, username, password, salt) VALUES(DEFAULT, $1, $2, $3) returning *";
+    let values = [username, password, salt];
+    return pool.query(sql, values); //return the promise
+}
+
+//-------------------------------
+dbMethods.deleteUser = function(id) {
+    let sql = "DELETE FROM users WHERE id = $1 RETURNING *";
+    let values = [id];
+    return pool.query(sql, values); //return the promise 
+}
+
+
+
 // export dbMethods ----------------------------
 module.exports = databaseMethods;
 
--- modules/users.js
+++ modules/users.js
@@ -7,20 +7,92 @@ const router = express.Router();
 
 // user login ---------------------------
 router.post("/users/login", async function(req, res, next) {
-   
+    /*
+    let credString = req.headers.authorization;
+    let cred = authUtils.decodeCred(credString);
+
+    if (cred.username == "" || cred.password == ""){
+        res.status(401).json({error: "no username og password"}).end();
+        return;
+    }
+
+    try {
+        let data = await database.getUser(cred.username);
+
+        if (data.rows.length > 0) {
+            let userid = data.rows[0].id;
+            let username = data.rows[0].username;
+            let userhashpassword = data.rows[0].password;
+            let usersalt = data.rows[0].salt;
+
+            if (authUtils.verifyPassword(cred.password, userhashpassword, usersalt)){
+
+                let tok = authUtils.createToken(username, userid);
+                res.status(200).json({
+                    msg: "The login was succesful.",
+                    token: tok
+                }).end();
+
+            }else{
+                res.status(401).json({msg: "Invalid password"}).end();
+                return;
+             }
+        }else {
+            res.status(403).end();
+            return;
+        }
+    }
+    catch (err) {
+        next(err);
+    }
+    */
 });
 
 // list all users -----------------------
 router.get("/users", async function(req, res, next){
 
     res.status(200).send("Hello from GET - /users").end();
-   
+    /*
+    try {
+        let data = await database.getAllUsers();
+        res.status(200).json(data.rows).end();
+    }
+    catch(err) {
+        next(err);
+    }
+    */
 });
 
 // create new user -----------------------
 router.post("/users", async function(req, res, next){
     
-      
+    /*
+
+    
+    let credString = req.headers.authorization;
+    let cred = authUtils.decodeCred(credString);
+
+    if (cred.username == "" || cred.password == ""){
+        res.status(401).json({error: "No username or password"}).end();
+        return;
+    }
+
+    let hash = authUtils.createHash(cred.password);
+
+    try {
+        let data = await database.createUser(cred.username, hash.value, hash.salt);
+
+        if (data.rows.length > 0) {
+            res.status(200).json({msg: "The user was created succesfully"}).end();
+        }
+        else{
+            throw "The user couldnt be created";
+        }
+    }
+    catch(err) {
+        next(err);
+    }
+*/    
 });
 
 // delete a user -----------------------
