928df82762bd4d485978509e9cea697f37e7dfdd
Håkbook Air <hshennig@live.no>
2021-12-06 13:12:06 +0100

    Lagt til muntlig.txt og begynt kommentering av auth_utils

--- modules/auth_utils.js
+++ modules/auth_utils.js
@@ -1,8 +1,11 @@
 const crypto = require("crypto");
-const secret = process.env.secret;
+const secret = process.env.secret;  // her brukes environment variable for å skjule "hemmeligheten", denne ble opprettet på herouk inne
+//på appen vår. 
 let utils = {};
 
 //Decode Credentials -----------------------------------------
+// Funksjon for å dekode credential-stringen som er laget på klienten.
+//
 utils.decodeCred = function (credString) {
   let cred = {};
 
@@ -22,16 +25,37 @@ utils.decodeCred = function (credString) {
 };
 
 //Create Hash -----------------------------------------
+// Her laged en "hash" ut av passordet. Det er nærmest umulig å ekstrahere passordet ut fra hash-en. 
+// En hash er en satt lengde med sifre. Og vi får igjen den samme stringen med sifre om vi setter inn det samme passordet igjen. 
+// Det er hashen som lagres i databasen, ikke passordet. 
+// Når brukeren logger inn lages det en hash ut ifra passordet. Om dette stemmer overens med hashen som ligger i databasen blir brukeren 
+// "godkjent", hvis ikke så får man beskjed om at det er feil passord. 
+
+// Viktig at både hashen og salten lagres i databasen så det er mulig å verifisere passordet når brukeren logger inn. 
 utils.createHash = function (password) {
   let hash = {};
 
-  hash.salt = Math.random().toString();
+  hash.salt = Math.random().toString(); // Genererer et tilfeldig tall å legge til i hashsen, for å "salte" det. 
   hash.value = crypto.scryptSync(password, hash.salt, 64).toString("hex");
 
   return hash;
 };
 
 // Create Token -----------------------------------------
+// Lager token for login, vi bruker en standard som heter "JSON web token (JWT)". 
+
+
+// Token er en text-string som består av tre deler: 
+// Part1 / HEADER: JSON som inneholder informasjon om hashing-algoritmen som brukes til å generere signaturen. 
+// Part 2 / PAYLOAD: JSON som inneholder informasjon (claims), som brukernavn, brukerID, og når token ble laget. 
+// Part 3 / SIGNATURE: Header og payload blir begge kodet inn i base64 også kombinert med et punktum mellom. Den blir så hashet med
+// krypteringsalgoritme som blir funnet i headeren. Den resulterende hashen blir så lagt til som den tredje delen av stringen etter et
+// punktum. 
+
+// Token blir så lagret på klienten (typisk i local storage, dette gjør vi og), også sent til serveren med autoriserings-headeren:
+// "authorization": token 
+
+
 utils.createToken = function (username, userID) {
   // part 1 and 2 as JSON-text
   let part1 = JSON.stringify({ alg: "HS256", typ: "JWT" });
@@ -59,6 +83,9 @@ utils.createToken = function (username, userID) {
 };
 
 // Verify Token -----------------------------------------
+// Når serveren tar i mot en token fra klienten, blir den verifisert ved å hashe header/payload (part1 & part2), også sjekker om resultatet
+// er det samme som signaturen. Om tokenen har en utløpsdato blir dette også sjekket her med Issued At Time-informasjonen "iat".
+
 utils.verifyToken = function (token) {
   //using the string-method split to extract the three parts into an array
   let tokenArr = token.split(".");
